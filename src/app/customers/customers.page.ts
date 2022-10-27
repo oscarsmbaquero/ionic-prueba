f1ec37d9f125d3100e623c938b22de27ce5c0406
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-10-27 17:38:57 +0200

    customers with avatar static and grid at home

--- src/app/customers/customers.page.ts
+++ src/app/customers/customers.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customers',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goToHome(){
+    this.router.navigate(['/home']);
   }
 
 }
